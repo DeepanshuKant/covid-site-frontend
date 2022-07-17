3a818bd270917672d8cdcb67da96401a90fbf4c1
myusername <myemail>
2022-07-17 16:45:22 +0530

    changed

--- src/components/Graph/Graph.js
+++ src/components/Graph/Graph.js
@@ -1,12 +1,21 @@
-import { ResponsiveContainer, BarChart, Bar, Cell, XAxis, YAxis, Legend, Tooltip } from 'recharts'
+// import { ResponsiveContainer, BarChart, Bar, Cell, XAxis, YAxis, Legend, Tooltip } from 'recharts'
 import { useSelector } from 'react-redux'
 import { useState, useEffect } from 'react'
+import { Doughnut } from 'react-chartjs-2'
+import { Chart, ArcElement, Tooltip, Title } from 'chart.js'
 
 function Graph() {
+
+    Chart.register(
+        ArcElement,
+        Title,
+        Tooltip
+    )
 
     const [allData, setAllData] = useState({})
     const reduxGlobal = useSelector((store) => store.global)
     const reduxCountry = useSelector((store) => store.countryReducer)
+
     useEffect(() => {
         function fetchData() {
             if (Object.keys(reduxCountry.country).length === 0 && reduxCountry.constructor === Object) {
@@ -20,52 +29,36 @@ function Graph() {
         fetchData();
     }, [reduxGlobal.global, reduxCountry.country]);
 
-    let newData = [
-        {
-            case: "TotalConfirmed",
-            value: allData.TotalConfirmed
-            // value: 20
-        },
-        {
-            case: "TotalDeaths",
-            value: allData.TotalDeaths
-            // value: 25
-        },
-        {
-            case: "TotalRecovered",
-            value: allData.TotalRecovered
-            // value: 15
-        }
-    ]
+
+    const newData = {
+        labels: [
+            'Total Confirmed',
+            'TotalDeaths',
+            'TotalRecovered'
+        ],
+        datasets: [{
+            label: 'Covid Data',
+            data: [allData.TotalConfirmed, allData.TotalDeaths, allData.TotalRecovered],
+            backgroundColor: [
+                'rgb(255, 99, 132)',
+                'rgb(54, 162, 235)',
+                'rgb(255, 205, 86)'
+            ],
+            hoverOffset: 4
+        }]
+    }
+
+    const config = {
+        type: 'doghnut',
+        data: newData,
+    };
+
+
     return (
         <>
-            <h1 style={{ color: "red", textAlign: "center", marginTop: "50px" }}>Data Representation</h1>
-            {/* <ResponsiveContainer width="90%" aspect={3}>
-                    <LineChart data={newData}>
-                        <XAxis dataKey="case" interval={'preserveStartEnd'} />
-                        <YAxis dataKey="value" />
-                        <Legend />
-                        <Line dataKey="value" stroke='red' />
-                    </LineChart>
-                </ResponsivedContainer> */}
-
-            <div className="graph__main__container">
-                <ResponsiveContainer aspect={3} >
-                    <BarChart data={newData} margin={{
-                        top: 5,
-                        right: 50,
-                        left: 50,
-                        bottom: 5,
-                    }}>
-                        <XAxis dataKey="case" fontSize="16px" />
-                        <YAxis dataKey="value" />
-                        <Tooltip />
-                        <Legend dataKey="value" />
-                        <Bar dataKey="value" fill="rgb(0, 106, 255)" />
-                    </BarChart>
-                </ResponsiveContainer>
+            <div className="main__parent" style={{ width: "30%", margin: "auto" }} >
+                <Doughnut data={newData} />
             </div>
-
         </>
     )
 }
